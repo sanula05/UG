bc0e2a412cb9d34654009a9de40d571908971265
Sanula1 <126245503+Sanula1@users.noreply.github.com>
2023-09-24 11:29:54 +0000

    function returns the necessary outputs

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc, asc
 from sqlalchemy.sql import func
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import networkx as nx
+import matplotlib.pyplot as plt
+import tube as t
+
+tube_map = nx.Graph()
+for station in t.tube_data.keys():
+    tube_map.add_node(station)
+
+for start_station, connections in t.tube_data.items():
+    for end_station, distance in connections.items():
+        tube_map.add_edge(start_station, end_station, weight=distance)
 
 app = Flask(__name__)
+app.secret_key = "hello_wrld"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+
 
 class UserDetails(db.Model):
   __tablename__ = "UserDetails"
@@ -46,10 +60,31 @@ def register():
 def index():
     return render_template('base.html')
 
-@app.route('/main')
-@login_required
+@app.route('/main',methods=['GET', 'POST'])
+# @login_required
 def main():
-    return render_template('main.html')
+   if request.method == 'POST':
+     start_point = request.form["start_point"]
+     end_point = request.form["end_point"]
+     session["start_point"] = start_point
+     session["end_point"] = end_point
+     return redirect(url_for("result"))
+   else:
+     return render_template('main.html')
+
+@app.route('/result')
+def result():
+  if "start_point" and "end_point" in session:
+    start_point = session["start_point"]
+    end_point = session["end_point"]
+    return(nx.dijkstra_path(tube_map,start_point,end_point))
+    
+    
+  else:
+   return redirect(url_for("main"))
+    
+  
+    # return render_template('result.html')
 
 @app.route('/login')
 def login():
